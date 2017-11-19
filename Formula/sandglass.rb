4990077f0423014437b38d901de15856b6762357
Celrenheit <celrenheit+github@gmail.com>
2017-11-19 01:48:07 +0100

    sandglass version v0.1.6

--- Formula/sandglass.rb
+++ Formula/sandglass.rb
@@ -1,9 +1,9 @@
 class Sandglass < Formula
   desc "Sandglass is a distributed, horizontally scalable, persistent, delayed message queue"
   homepage "https://github.com/celrenheit/sandglass"
-  url "https://github.com/celrenheit/sandglass/releases/download/v0.1.5/sandglass_0.1.5_macOS_64-bit.tar.gz"
-  version "0.1.5"
-  sha256 "46089d4efa387ba9be424acade0625947c049dd901452ffee47743e8ce1223a7"
+  url "https://github.com/celrenheit/sandglass/releases/download/v0.1.6/sandglass_0.1.6_macOS_64-bit.tar.gz"
+  version "0.1.6"
+  sha256 "44831d3b50f4e30d4309a758c1e985557492ef00e4955275308038af880eb2eb"
 
   def install
     bin.install "sandglass"
