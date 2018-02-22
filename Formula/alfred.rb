772c70a85326cd104bc353341327b8714f0a34b7
Celrenheit <celrenheit+github@gmail.com>
2018-02-22 09:40:20 +0100

    alfred version v0.1.4

--- Formula/alfred.rb
+++ Formula/alfred.rb
@@ -1,9 +1,9 @@
 class Alfred < Formula
   desc "A user friendly Stellar wallets manager."
   homepage "https://github.com/celrenheit/alfred"
-  url "https://github.com/celrenheit/alfred/releases/download/v0.1.3/alfred_0.1.3_macOS_64-bit.tar.gz"
-  version "0.1.3"
-  sha256 "54bab73ee555532319bb0a882a6e668b184ee39056cb864313729fcd05619f2e"
+  url "https://github.com/celrenheit/alfred/releases/download/v0.1.4/alfred_0.1.4_macOS_64-bit.tar.gz"
+  version "0.1.4"
+  sha256 "2810752f9ffa0b1ff5ee744f850ad8cd3081670c7657140d8cfaf83074c77f7e"
 
   def install
     bin.install "alfred"
